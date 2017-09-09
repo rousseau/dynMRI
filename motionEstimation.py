3267e106d64f30ca05064a3e3af93330e65ab1d7
k16makki <karim.makki@telecom-bretagne.fr>
2017-09-09 14:05:29 +0200

    Reviewed version

--- motionEstimation.py
+++ motionEstimation.py
@@ -1,3 +1,40 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+
+
+  © IMT Atlantique - LATIM-INSERM UMR 1101
+
+  Author(s): Karim Makki (karim.makki@imt-atlantique.fr)
+
+  This software is governed by the CeCILL-B license under French law and
+  abiding by the rules of distribution of free software.  You can  use,
+  modify and/ or redistribute the software under the terms of the CeCILL-B
+  license as circulated by CEA, CNRS and INRIA at the following URL
+  "http://www.cecill.info".
+
+  As a counterpart to the access to the source code and  rights to copy,
+  modify and redistribute granted by the license, users are provided only
+  with a limited warranty  and the software's author,  the holder of the
+  economic rights,  and the successive licensors  have only  limited
+  liability.
+
+  In this respect, the user's attention is drawn to the risks associated
+  with loading,  using,  modifying and/or developing or reproducing the
+  software by the user in light of its specific status of free software,
+  that may mean  that it is complicated to manipulate,  and  that  also
+  therefore means  that it is reserved for developers  and  experienced
+  professionals having in-depth computer knowledge. Users are therefore
+  encouraged to load and test the software's suitability as regards their
+  requirements in conditions enabling the security of their systems and/or
+  data to be ensured and,  more generally, to use and operate it in the
+  same conditions as regards security.
+
+  The fact that you are presently reading this means that you have had
+  knowledge of the CeCILL-B license and that you accept its terms.
+
+"""
+
 import os
 import os.path
 import glob
@@ -5,15 +42,52 @@ import numpy as np
 from numpy import matrix
 from numpy.linalg import inv
 import nibabel as nib
-import matplotlib.pyplot as plt
-from pylab import *
 import nipype.algorithms.metrics as nipalg
 import argparse
 import multiprocessing
 
+#import matplotlib.pyplot as plt
+#from pylab import *
 
-#@author: Karim
 
+
+# Read from text file and store in matrix
+#
+# Parameters
+# ----------
+# filename : text filename (.mat)
+#
+# Returns matrix of floats
+# -------
+# output :
+# 4*4 transformation matrix
+
+
+def Text_file_to_matrix(filename):
+   T = np.loadtxt(str(filename), dtype='f')
+   return np.mat(T)
+
+# save matrix as text file
+#
+# Parameters
+# ----------
+# matrix : the matrix to be saved as text file
+# text_filename : desired name of the text file (.mat)
+
+def Matrix_to_text_file(matrix, text_filename):
+    np.savetxt(text_filename, matrix, delimiter='  ')
+
+# compute the dice score between two binary masks
+#
+# Parameters
+# ----------
+# rfile : reference image filename
+# ifile : input image filename
+#
+# Returns
+# -------
+# output : scalar
+# dice score between the two masks
 
 # compute the normalized correlation between image 1 and image 2 using binary mask
 #
@@ -49,40 +123,6 @@ def Normalized_cross_Correlation(image1, image2, mask):
 
     return (normalized_cross_correlation)
 
-
-
-# convert a text file to matrix
-#
-# Parameters
-# ----------
-# filename : text filename (.mat)
-#
-# Returns matrix of floats
-# -------
-# output :
-# 4*4 transformation matrix
-
-
-def Text_file_to_matrix(filename):
-   T = np.loadtxt(str(filename), dtype='f')
-   return np.mat(T)
-#
-
-def Matrix_to_text_file(matrix, text_filename):
-    np.savetxt(text_filename, matrix, delimiter='  ')
-#
-
-# compute the dice score between two binary masks
-#
-# Parameters
-# ----------
-# rfile : reference image filename
-# ifile : input image filename
-#
-# Returns
-# -------
-# output : scalar
-# overlap between the two masks
 
 def Bin_dice(rfile,ifile):
 
@@ -122,6 +162,7 @@ def Bin_dice(rfile,ifile):
 
 
 def Fuzzy_dice(rfile, ifile):
+
     overlap = nipalg.FuzzyOverlap()
     overlap.inputs.in_ref = rfile
     overlap.inputs.in_tst = ifile
@@ -146,29 +187,30 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
 
-##############Output path creation###################################
+#######################Output path creation##########################
 
     outputpath=args.output
     if not os.path.exists(outputpath):
        os.makedirs(outputpath)
 
-##############Data###################################
+############################## Data #################################
+
     High_resolution_static = args.static # 3D image
     dynamic = args.dyn  # 4D volume
 
-#############Split 4D image into a set of 3D image using fsl5.0-fslsplit##############
+############# split the 4D file into lots of 3D file s###############
+
     output_basename = 'dyn'
     go = 'fsl5.0-fslsplit '+dynamic+' '+outputpath+output_basename
-    print(go)
     os.system(go)
 
-    #Get the set of 3D time frames
+############ Get the sorted set of 3D time frames ###################
+
     dynamicSet = glob.glob(outputpath+'/'+output_basename+'*.nii.gz')
     dynamicSet.sort()
 
-#
-#    ###### Automated folders creation ##############
-#
+################# Automated folders creation ########################
+
     outputpath_bone=outputpath+'propagation'
     if not os.path.exists(outputpath_bone):
         os.makedirs(outputpath_bone)
@@ -182,104 +224,107 @@ if __name__ == '__main__':
     outputpath_boneSet=glob.glob(outputpath_bone+'/*')
     outputpath_boneSet.sort()
 
+########################### Notations ################################
+# t describes the time
+# i describes the the component or the bone
+######################################################################
+
+####################### Define basenames #############################
 
     global_mask_basename = 'global_mask'
     global_image_basename = 'flirt_global_static'
     global_matrix_basename = 'global_static_on'
-    local_matrix_basename = 'transform_global_on_dyn'
+    local_matrix_basename = 'transform_dyn'
     mask_basename = 'mask_dyn'
-    direct_transform_basename='direct_static_on'
+    direct_transform_basename = 'direct_static_on'
+    propagation_matrix_basename = 'matrix_flirt'
+    propagation_image_basename = 'flirt_dyn'
 
-##    ##############################################Global registration of the static on each time frame ############################################################################
+######## Global registration of the static on each time frame #########
+
+    movimage= High_resolution_static
     for t in range(0, len(dynamicSet)):
-
         refimage = dynamicSet[t]
-        movimage= High_resolution_static
         prefix = dynamicSet[t].split('/')[-1].split('.')[0]
         global_outputimage = outputpath+'flirt_global_static_on_'+prefix+'.nii.gz'
-        global_outputmat = outputpath+'global_static_on_'+prefix+'_.mat'
-
-##             ####Global rigid registration of dyn0 on static
-
-        go_init = 'time flirt -noresampblur -searchrx -40 40 -searchry -40 40 -searchrz -40 40  -dof 6 -ref '+refimage+' -in '+movimage+' -out '+global_outputimage+' -omat '+global_outputmat
+        global_outputmat = outputpath+'global_static_on_'+prefix+'.mat'
+        go_init = 'flirt -noresampblur -searchrx -40 40 -searchry -40 40 -searchrz -40 40  -dof 6 -ref '+refimage+' -in '+movimage+' -out '+global_outputimage+' -omat '+global_outputmat
         jobs.append(go_init)
     pool.map(os.system,jobs)
-#    #exit(1)
+
+#########################################################################
 
     global_matrixSet=glob.glob(outputpath+global_matrix_basename+'*.mat')
     global_matrixSet.sort()
-##
+
     global_imageSet=glob.glob(outputpath+global_image_basename+'*.nii.gz')
     global_imageSet.sort()
-##
-##################Propagate segmentations into the low_resolution domain #################################################################################
+
+##### Propagate manual segmentations into the low_resolution domain #####
+############# using the global etimated transformations #################
+
     for i in range(0,len(outputpath_boneSet)):
         for t in range(0, len(global_imageSet)):
-
             prefix = dynamicSet[t].split('/')[-1].split('.')[0]
             global_mask= outputpath_boneSet[i]+'/global_mask_'+prefix+'_component_'+str(i)+'.nii.gz'
-            go_propagation = 'time flirt -applyxfm -noresampblur -ref '+global_imageSet[t]+' -in ' + args.mask[i] + ' -init '+ global_matrixSet[t] + ' -out ' + global_mask + ' -interp nearestneighbour '
+            go_propagation = 'flirt -applyxfm -noresampblur -ref '+global_imageSet[t]+' -in ' + args.mask[i] + ' -init '+ global_matrixSet[t] + ' -out ' + global_mask + ' -interp nearestneighbour '
             jobs.append(go_propagation)
     pool.map(os.system,jobs)
 
+##########################################################################
 
-#    ####Local Rigid registration of bones########
-#
+##############Local Rigid registration of bones###########################
+
     for i in range(0,len(outputpath_boneSet)):
-#
+
         global_maskSet=glob.glob(outputpath_boneSet[i]+'/'+global_mask_basename+'*.nii.gz')
         global_maskSet.sort()
-#
+
         for t in range(0, len(dynamicSet)):
+
             prefix = dynamicSet[t].split('/')[-1].split('.')[0]
             refimage = dynamicSet[t]
-            local_outputimage = outputpath_boneSet[i]+'/flirt_global_on_'+prefix+'_component_'+str(i)+'.nii.gz'
-            local_outputmat = outputpath_boneSet[i]+'/transform_global_on_'+prefix+'_component_'+str(i)+'.mat'
-            #go_init = 'time flirt -searchrx -40 40 -searchry -40 40 -searchrz -40 40 -dof 6 -in '+refimage+' -ref '+global_imageSet[t]+' -out '+local_outputimage+' -omat '+local_outputmat +' -refweight '+ global_maskSet[t]
-            go_init = 'time flirt  -searchrx -40 40 -searchry -40 40 -searchrz -40 40 -searchcost normcorr -dof 6 -in '+global_imageSet[t]+' -ref '+refimage+' -out '+local_outputimage+' -omat '+local_outputmat +' -inweight '+ global_maskSet[t]
+            local_outputimage = outputpath_boneSet[i]+'/flirt_'+prefix+'_on_global_component_'+str(i)+'.nii.gz'
+            local_outputmat = outputpath_boneSet[i]+'/transform_'+prefix+'_on_global_component_'+str(i)+'.mat'
+            go_init = 'flirt -searchrx -40 40 -searchry -40 40 -searchrz -40 40  -anglerep quaternion -dof 6 -in '+refimage+' -ref '+global_imageSet[t]+' -out '+local_outputimage+' -omat '+local_outputmat +' -refweight '+ global_maskSet[t]
+
+###################### Tibia registration ################################
+            if(i==2):
+                go_init = 'flirt  -searchrx -40 40 -searchry -40 40 -searchrz -40 40  -dof 6 -in '+global_imageSet[t]+' -ref '+refimage+' -out '+local_outputimage+' -omat '+local_outputmat +' -inweight '+ global_maskSet[t]
+##########################################################################
             jobs.append(go_init)
     pool.map(os.system,jobs)
 
-#### compute local transformations from static to each time frame ################
+#### Compute composed transformations from static to each time frame #####
+
     for i in range(0,len(outputpath_boneSet)):
-#
-        local_matrixSet=glob.glob(outputpath_boneSet[i]+'/'+local_matrix_basename+'*.mat')
+        local_matrixSet = glob.glob(outputpath_boneSet[i]+'/'+local_matrix_basename+'*.mat')
         local_matrixSet.sort()
-
         for t in range(0, len(dynamicSet)):
-
             prefix = dynamicSet[t].split('/')[-1].split('.')[0]
-            link_matrix=np.dot(Text_file_to_matrix(local_matrixSet[t]), Text_file_to_matrix(global_matrixSet[t]))
+            static_to_dyn_matrix = np.dot(np.linalg.inv(Text_file_to_matrix(local_matrixSet[t])), Text_file_to_matrix(global_matrixSet[t]))
+            if(i==2):
+                static_to_dyn_matrix = np.dot(Text_file_to_matrix(local_matrixSet[t]), Text_file_to_matrix(global_matrixSet[t]))
+
             save_path= outputpath_boneSet[i]+'/direct_static_on_'+prefix+'_component_'+str(i)+'.mat'
-            np.savetxt(save_path,link_matrix, delimiter='  ')
-######Propagate the high_resolution segmentations into time frames #############################
+            np.savetxt(save_path,static_to_dyn_matrix, delimiter='  ')
+
+###### Propagate the high_resolution segmentations into time frames #######
+####### using the estimated composed transformations from static ##########
+########################## to each time frame #############################
 
     for i in range(0,len(outputpath_boneSet)):
-        init_matrixSet=glob.glob(outputpath_boneSet[i]+'/'+direct_transform_basename+'*.mat')
+        init_matrixSet = glob.glob(outputpath_boneSet[i]+'/'+direct_transform_basename+'*.mat')
         init_matrixSet.sort()
-
         for t in range(0, len(dynamicSet)):
             prefix = dynamicSet[t].split('/')[-1].split('.')[0]
-            out2= outputpath_boneSet[i]+'/mask_'+prefix+'_component_'+str(i)+'.nii.gz'
-            go_init = 'time flirt -applyxfm -noresampblur -ref '+ dynamicSet[t] + ' -in '+ args.mask[i] + ' -out '+ out2 + ' -init '+init_matrixSet[t]+ ' -interp nearestneighbour '
+            low_resolution_mask = outputpath_boneSet[i]+'/mask_'+prefix+'_component_'+str(i)+'.nii.gz'
+            go_init = 'time flirt -applyxfm -noresampblur -ref '+ dynamicSet[t] + ' -in '+ args.mask[i] + ' -out '+ low_resolution_mask + ' -init '+init_matrixSet[t]+ ' -interp nearestneighbour '
             jobs.append(go_init)
     pool.map(os.system,jobs)
 
-    ############ Aplly local transformations to the static image  ###########
+############ Aplly local transformations to the static image  ##############
 
-#    for i in range(0,len(outputpath_boneSet)):
-#        init_matrixSet=glob.glob(outputpath_boneSet[i]+'/'+direct_transform_basename+'*.mat')
-#        init_matrixSet.sort()
-#        for t in range(0, len(dynamicSet)):
-#            prefix = dynamicSet[t].split('/')[-1].split('.')[0]
-#            static_on_dyn0= outputpath_boneSet[i]+'/direct_static_on_'+prefix+'_component_'+str(i)+'.nii.gz'
-#            go = 'time flirt -applyxfm -noresampblur -ref '+ dynamicSet[t] + ' -in '+ High_resolution_static + ' -out '+ static_on_dyn0 + ' -init '+init_matrixSet[t]
-#            print(go)
-#            jobs.append(go)
-#    pool.map(os.system,jobs)
-#
-
- ##Fast version:
     for i in range(0,len(outputpath_boneSet)):
         local_matrixSet=glob.glob(outputpath_boneSet[i]+'/'+local_matrix_basename+'*.mat')
         local_matrixSet.sort()
@@ -287,12 +332,12 @@ if __name__ == '__main__':
             prefix = dynamicSet[t].split('/')[-1].split('.')[0]
             static_on_dyn0= outputpath_boneSet[i]+'/direct_static_on_'+prefix+'_component_'+str(i)+'.nii.gz'
             go = 'time flirt -applyxfm -ref '+ dynamicSet[t] + ' -in '+ global_imageSet[t] + ' -out '+ static_on_dyn0 + ' -init '+local_matrixSet[t]
-            #print(go)
             jobs.append(go)
     pool.map(os.system,jobs)
+    pool.close()
+    pool.join()
 
-
-    ################ finding the time frame that best align with static image  #####################################################################################################################
+######### Finding the time frame that best align with static image  ########
 
     dice_evaluation_array=np.zeros((len(outputpath_boneSet), len(dynamicSet)))
     normalized_correlation_evaluation_array=np.zeros((len(outputpath_boneSet), len(dynamicSet)))
@@ -306,21 +351,22 @@ if __name__ == '__main__':
         direct_static_on_dynSet=glob.glob(outputpath_boneSet[i]+'/'+direct_transform_basename+'*.nii.gz')
         direct_static_on_dynSet.sort()
 
-
         for t in range(0, len(dynamicSet)):
+
             normalized_correlation_evaluation_array[i][t] = Normalized_cross_Correlation(direct_static_on_dynSet[t], dynamicSet[t], maskSet[t])
             dice_evaluation_array[i][t] = Bin_dice(maskSet[t],global_maskSet[t])
 
     linked_time_frame=np.prod(dice_evaluation_array, axis=0)
-    t=np.argmax(linked_time_frame)
-    if(t<0.5):
+    t=np.argmax(linked_time_frame) ### the main idea here is to detect the time frame the most closest to the static scan ("no-motion" detection)
+
+    if(t<0.6):
         linked_time_frame=np.prod(normalized_correlation_evaluation_array, axis=0)
         t=np.argmax(linked_time_frame)
 
-    ############### Propagate segmentations into the Low-resolution domain ####################################
+########## copy the best "static/dynamic" registration results to the final outputs folder ############
 
     for i in range(0,len(outputpath_boneSet)):
-        ###create a folder to save final results#######
+
         maskSet=glob.glob(outputpath_boneSet[i]+'/'+mask_basename+'*.nii.gz')
         maskSet.sort()
         transformationSet=glob.glob(outputpath_boneSet[i]+'/'+direct_transform_basename+'*.mat')
@@ -330,18 +376,19 @@ if __name__ == '__main__':
         if not os.path.exists(output_results):
             os.makedirs(output_results)
 
-        t=np.argmax(linked_time_frame) #temporal position of the best aligned time frame with the static
+        t=np.argmax(linked_time_frame)
 
         copy= 'cp '+maskSet[t]+ '  '+ output_results
         os.system(copy) ######copy mask(t) to the final_results folder
         copy= 'cp '+transformationSet[t]+ '  '+ output_results
-        os.system(copy) ######copy the transformation that links between static and dynamic data
+        os.system(copy) ######copy the most accurate estimated transformation in the "final_results" folder
+
         direct_static_on_dynSet=glob.glob(output_results+'/'+direct_transform_basename+'*.mat')
         direct_static_on_dynSet.sort()
         final_refweightSet=glob.glob(output_results+'/'+mask_basename+'*.nii.gz')
         final_refweightSet.sort()
 
-    ########Backward propagation ###############################
+######################## Backward propagation ##############################
 
         while(t>0):
 
@@ -371,7 +418,8 @@ if __name__ == '__main__':
             t-=1
 
 
-    ########Forward propagation ###############################
+######################### Forward propagation ##############################
+
         direct_static_on_dynSet=glob.glob(output_results+'/'+direct_transform_basename+'*.mat')
         direct_static_on_dynSet.sort()
         final_refweightSet=glob.glob(output_results+'/'+mask_basename+'*.nii.gz')
@@ -409,3 +457,28 @@ if __name__ == '__main__':
             final_refweightSet.sort()
 
             t+=1
+
+
+########## Clean "final_results" folders inorder to only keep the low_resolution automated segmentations and the direct transformations from the static scan to each low-resolution time frame ########################
+####################################  move all intermediate results from the "final_results" folder to the "outputpath_boneSet[i]" folder #############################################################################
+
+    for i in range(0,len(outputpath_boneSet)):
+
+
+        output_results=outputpath_boneSet[i]+'/final_results'
+
+        propagation_matrixSet=glob.glob(output_results+'/'+propagation_matrix_basename+'*.mat')
+        propagation_matrixSet.sort()
+
+        propagation_imageSet=glob.glob(output_results+'/'+propagation_image_basename+'*.nii.gz')
+        propagation_imageSet.sort()
+
+        for t in range(0, len(propagation_matrixSet)):
+
+            prefix1 = dynamicSet[t].split('/')[-1].split('.')[0]
+            prefix2 = dynamicSet[t+1].split('/')[-1].split('.')[0]
+
+            go1= 'mv '+ propagation_matrixSet[t] +' '+ outputpath_boneSet[i]+'/matrix_flirt_'+prefix1+'_on_'+prefix2+'.mat'
+            go2= 'mv '+ propagation_imageSet[t] +' '+ outputpath_boneSet[i]+'/flirt_'+prefix1+'_on_'+prefix2+'.nii.gz'
+            os.system(go1)
+            os.system(go2)
