c526ef0fcb3dc9d4d961831d4e1c99f2d135bcc5
Yue Cheng <yue.cheng.chloe@gmail.com>
2021-09-24 17:20:07 +0200

    scaling changed

--- deformity analysis/mesh_generation.py
+++ deformity analysis/mesh_generation.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
         bone_mesh = os.path.join('.', b, b + '.stl')
         subjects = glob.glob(os.path.join('.', b, 'results', '*.nii.gz'))
         for s in subjects:
-            texture = 'python texture_plugger_new.py -s BSpline -is ' + bone_mesh + ' -it ' + s + ' -o ' \
+            texture = 'python texture_plugger.py --scaling 100 -i bspline -s ' + bone_mesh + ' -t ' + s + ' -o ' \
                       + os.path.join('.', b, 'results', 'mesh', s.rsplit('/', 1)[1].split('.')[0] + '.ply')
             print(texture)
             os.system(texture)
