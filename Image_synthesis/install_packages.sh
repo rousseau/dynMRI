585dd8b9650c4b7dfef8e797de45346f490a5969
Claire Scavinner--Dorval <73347729+cScavinner@users.noreply.github.com>
2023-09-25 11:53:05 +0200

    Create install_packages.sh

---
+++ Image_synthesis/install_packages.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+conda install lightning -c conda-forge
+conda install -c conda-forge torchio
+conda install -c conda-forge matplotlib
+conda install -c conda-forge tensorboard
